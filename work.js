eb9ea8eee3d8027cfbbc097704a2b3417c90b3c7
david221104 <davidkrdzo.2@gmail.com>
2024-11-22 16:30:03 +0100

    finish getHumanChoice function

--- work.js
+++ work.js
@@ -1,9 +1,9 @@
 // random number function
 function getComputerChoice() {
-    //gets a random number
+    // gets a random number
     let number = Math.floor(Math.random() * 3); 
     console.log(number);
-    //decides rock, paper or scissors based on the given number
+    // decides rock, paper or scissors based on the given number
     switch(number) {
         case 0 : console.log("Rock!")                    
                     break;
@@ -16,3 +16,12 @@ function getComputerChoice() {
 }
 
 getComputerChoice();
+
+// prompts for player input 
+function getHumanChoice() {
+    let choice = prompt("Rock, paper or scissor ?");
+    console.log(choice);
+}
+
+getHumanChoice();
+
